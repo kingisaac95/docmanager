d325a60b2ef4732f160c7ead999c6f6c049ffdfa
Orjiewuru Kingdom <kingdom.orjiewuru@andela.com>
2017-05-25 13:42:32 +0100

    change config file format

--- src/server/models/index.js
+++ src/server/models/index.js
@@ -4,7 +4,7 @@ import Sequelize from 'sequelize';
 
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.json')[env];
+const config = require('../config/config')[env];
 
 const db = {};
 
